2553a4d056784fc1d59e3388fde85ef24adbae2f
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2024-04-08 00:02:36 +0530

    restricing exp access
    using this session user can access expirement only on login or signup .It is implemented to p1 only as of now .Implemented using express-session

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // module
-const express = require('express');
+const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
 const conn = require("./db/mongodb");
@@ -14,113 +14,146 @@ const DensitySonometer = require("./models/densitySonometer");
 const ThicknessSheet = require("./models/thicknessSheet");
 const TwoJunction = require("./models/twoJunction");
 //var popup = require('popups');
-const alert = require('alert'); 
-const bcrypt = require('bcrypt');
+const alert = require("alert");
+const bcrypt = require("bcrypt");
+var session = require("express-session");
 // Creating express object
 const app = express();
 
-
-const staticPath = path.join(__dirname, '../public');
-const templatePath = path.join(__dirname, '../public/templates/views');
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../public/templates/views");
 
 app.use(express.static(staticPath));
 app.set("view engine", "hbs");
 app.set("views", templatePath);
 app.use(express.json());
 
-app.use(express.urlencoded({
-  extended: false
-}));
-
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days in milliseconds
+    },
+  })
+);
 
 app.get("/", (req, res) => {
-  res.render("proceed")
-})
+  res.render("proceed");
+});
 //list of expreriments
 app.get("/exp", (req, res) => {
-  res.render("home")
-})
+  res.render("home");
+});
 app.get("/login", (req, res) => {
-  res.render("login")
-})
+  res.render("login");
+});
 app.get("/signup", (req, res) => {
-  res.render("signup")
-})
+  res.render("signup");
+});
 
 // app.get("/home", (req, res) => {
 //   res.render("home")
 // })
 
 app.get("/p1", (req, res) => {
-  res.render("p1")
-})
+  if (req.session.user) {
+    res.render("p1");
+  } else {
+    res.send("Please login to continue..")
+  }
+});
 app.get("/p2", (req, res) => {
-  res.render("p2")
-})
+  res.render("p2");
+});
 
 app.get("/p3", (req, res) => {
-  res.render("p3")
-})
+  res.render("p3");
+});
 app.get("/p4", (req, res) => {
-  res.render("p4")
-})
+  res.render("p4");
+});
 app.get("/p5", (req, res) => {
-  res.render("p5")
-})
+  res.render("p5");
+});
 app.get("/p5", (req, res) => {
-  res.render("p5")
-})
+  res.render("p5");
+});
 
 app.get("/p6", (req, res) => {
-  res.render("p6")
-})
+  res.render("p6");
+});
 
 app.get("/p7", (req, res) => {
-  res.render("p7")
-})
+  res.render("p7");
+});
 
 app.get("/p8", (req, res) => {
-  res.render("p8")
-})
+  res.render("p8");
+});
 
 app.get("/p9", (req, res) => {
-  res.render("p9")
-})
+  res.render("p9");
+});
 
 app.post("/signup", async (req, res) => {
-  const bcrypt = require('bcrypt');
+  const bcrypt = require("bcrypt");
   const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(req.body.password,saltRounds);
+  const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
   const registerUser = new Register({
     name: req.body.name,
     email: req.body.email,
-    password: hashedPassword
-  })
+    password: hashedPassword,
+  });
 
-  try{
-    const registered = await registerUser.save()
-  }catch(error){
-    console.log("Error in signing in:",error.message)
+  try {
+    const registered = await registerUser.save();
+    req.session.user = {
+      name: req.body.name,
+      email: req.body.email,
+      // Add other user properties if needed
+    };
+  } catch (error) {
+    console.log("Error in signing in:", error.message);
     res.send("Error in signup");
   }
+  
   //const registered = await registerUser.save();
   //res.send("Registered Successfully");
   //alert("Registered Successfully");
-  
 
   res.render("home");
-})
+});
 
 app.post("/login", async (req, res) => {
   try {
     const check = await Register.findOne({
-      email: req.body.email
-    })
-    const match = await bcrypt.compare(req.body.password,check.password);
+      email: req.body.email,
+    });
+    const match = await bcrypt.compare(req.body.password, check.password);
+
+    if (match) {
+      // Store user information in session
+      req.session.user = {
+        name: check.name,
+        email: check.email,
+        // Add other user properties if needed
+      };
+      res.render("proceed");
+    } else {
+      res.send("Please login to access this page");
+    }
+
     if (match) {
       res.render("proceed");
     } else {
-      res.send("Wrong password")
+      res.send("Wrong password");
     }
 
     // if (check.password === req.body.password) {
@@ -128,11 +161,11 @@ app.post("/login", async (req, res) => {
     // } else {
     //   res.send("Wrong password")
     // }
-  } catch(error) {
-    console.error(error)
-    res.send("Wrong details//user does not exit")
+  } catch (error) {
+    console.error(error);
+    res.send("Wrong details//user does not exit");
   }
-})
+});
 
 // adding data to database
 
@@ -144,86 +177,76 @@ app.post("/p1", async (req, res) => {
     current: req.body.current,
     thermometer: req.body.thermometer,
     voltage: req.body.voltage,
-    temperature: req.body.temperature
-
-  })
+    temperature: req.body.temperature,
+  });
   // //giving data to mongo db
   const data_added = await crystal.save();
-
-})
+});
 
 // for p2
 app.post("/p2", async (req, res) => {
   const ohms = new OhmsLaw({
     resistance: req.body.resistance,
-    voltage: req.body.voltage
-  })
+    voltage: req.body.voltage,
+  });
   // //giving data to mongo db
   const data_added = await ohms.save();
-
-})
+});
 // for p3
 app.post("/p3", async (req, res) => {
   const energyEquivalence = new EnergyEquivalence({
     speedOfLight: req.body.speedOfLight,
-    mass: req.body.mass
-  })
+    mass: req.body.mass,
+  });
   // //giving data to mongo db
   const data_added = await energyEquivalence.save();
-
-})
+});
 
 // for p4
 app.post("/p4", async (req, res) => {
   const meterBridge = new MeterBridge({
     diameter: req.body.diameter,
     resistance: req.body.resistance,
-    length: req.body.length
-  })
+    length: req.body.length,
+  });
   // //giving data to mongo db
   const data_added = await meterBridge.save();
-
-})
-
+});
 
 // for p5
 app.post("/p5", async (req, res) => {
-  try{
-  const seriesResistance = new SeriesResistance({
-    resistance: req.body.resistance,
-    balance: req.body.balance
-  })
-  // //giving data to mongo db
-  
-    const data_added = await seriesResistance.save();
-  }catch(error){
-    console.log("Error in databse of P5",error.message)
-  }
- 
+  try {
+    const seriesResistance = new SeriesResistance({
+      resistance: req.body.resistance,
+      balance: req.body.balance,
+    });
+    // //giving data to mongo db
 
-})
+    const data_added = await seriesResistance.save();
+  } catch (error) {
+    console.log("Error in databse of P5", error.message);
+  }
+});
 
 // for p6
 app.post("/p6", async (req, res) => {
   const compareEmf = new CompareEmf({
     l1: req.body.l1,
-    l2: req.body.l2
-  })
+    l2: req.body.l2,
+  });
   // //giving data to mongo db
   const data_added = await compareEmf.save();
-
-})
+});
 
 // for p7
 app.post("/p7", async (req, res) => {
   const densitySonometer = new DensitySonometer({
     l1: req.body.l1,
-    l2: req.body.l2
-  })
+    l2: req.body.l2,
+  });
   // //giving data to mongo db
   const data_added = await densitySonometer.save();
-
-})
+});
 
 // for p8
 app.post("/p8", async (req, res) => {
@@ -231,12 +254,11 @@ app.post("/p8", async (req, res) => {
     leastcount: req.body.leastcount,
     zeroerror: req.body.zeroerror,
     linearscale: req.body.linearscale,
-    circularscale: req.body.circularscale
-  })
+    circularscale: req.body.circularscale,
+  });
   // //giving data to mongo db
   const data_added = await thicknessSheet.save();
-
-})
+});
 
 // for p9
 app.post("/p9", async (req, res) => {
@@ -246,13 +268,11 @@ app.post("/p9", async (req, res) => {
     resistance_resistancebox: req.body.resistance_resistancebox,
     length_potentiometer: req.body.length_potentiometer,
     temperature: req.body.temperature,
-    length: req.body.length
-
-  })
+    length: req.body.length,
+  });
   // //giving data to mongo db
   const data_added = await twoJunction.save();
-
-})
+});
 app.listen(3000, () => {
-  console.log("Port Connected")
-})
+  console.log("Port Connected");
+});
